120bdc8cdd1a86dcbc1858be23cf9b74a07cbafd
Matheus Nicéas <niceas49@hotmail.com>
2020-03-09 16:42:51 -0300

    Edição e melhoria do processo de update.

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-eval apt-get -y update && apt-get -y upgrade
+#eval apt-get -y update && apt-get -y upgrade
+eval apt-get -y update
+echo ""
+echo "UPDATE OK"
+echo ""
+eval apt-get -y upgrade && apt-get autoremove
+echo ""
+echo "UPGRADE OK"
+echo ""
 eval apt-get -y install -f
+echo ""
+echo "Muito bem, $USER! O seu sistema está atualizado."
